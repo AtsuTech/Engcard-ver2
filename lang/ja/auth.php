7d4ec5faa59724dd7e30d1081acc87000ffee27b
ahitotsui <atsushiabc1204@gmail.com>
2024-09-28 13:34:30 +0900

    update: エラーテキスト日本語化

---
+++ lang/ja/auth.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    // 'failed' => 'These credentials do not match our records.',
+    // 'password' => 'The provided password is incorrect.',
+    // 'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    'failed' => 'ご入力されたメールアドレスが間違っている可能性があります',
+    'password' => 'パスワードが間違っています',
+    'throttle' => 'ログイン試行回数が多いです。:seconds 後に再度お試しください',
+
+];
