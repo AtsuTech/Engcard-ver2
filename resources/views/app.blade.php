81dda926740c73fccc6dc25918563bb2f6015c64
ahitotsui <atsushiabc1204@gmail.com>
2024-10-14 11:11:49 +0900

    update:ファビコン設定

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,9 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- favicon (resources>views>components>favicon.blade.phpから呼び出し)) -->
+        @component('components.favicon')@endcomponent
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
